42d03a7f8d818114ca5dbfd1ee70d49f8858d983
TrailBl4zer <31038145+TrailBl4zer@users.noreply.github.com>
2017-10-26 16:37:22 +0200

    Fixing the goofiness

--- src/main/java/com/mysite/selenium/Weyland.java
+++
@@ -1,8 +0,0 @@
-package com.mysite.selenium;
-
-public class Weyland {
- public static final String LOC_BOX_USERNAME = "input[name=\"user\"]";
- public static final String LOC_BOX_PASSWORD = "input[name=\"pass\"]";
- public static final String LOC_BTN_CONNECT  = "input[type=\"submit\"]";
- public static final String LOC_MSG_CONNECT  = "#login_message";
-}
